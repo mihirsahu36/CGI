e1a16d7efb65bca3e0333fc3fca01af1e2d4e240
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2024-08-06 09:33:32 +0530

    Update practice.c

--- practice.c
+++ practice.c
@@ -423,3 +423,89 @@ void main(int argc, char **argv)
     glEnable(GL_DEPTH_TEST);
     glutMainLoop();
 }
+
+
+
+
+#include<GL/glut.h>
+#include<Stdio.h>
+int axis;
+int theta;
+int ch=1;
+
+void mouse(int button,int state,int x,int y){
+  if(button==GLUT_LEFT_BUTTON && state==GLUT_DOWN){
+    axis=0;
+  }
+  if(button==GLUT_MIDDLE_BUTTON && state==GLUT_DOWN){
+    axis=1;
+  }
+  if(button==GLUT_RIGHT_BUTTON && state==GLUT_UP){
+    axis=2;
+  }
+}
+
+void idle(){
+  int theta[axis]+=2;
+  if(theta[axis}>300){
+    theta[axis]=0;
+  }
+  glutRepostDisplay();
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(1,1,1,1);
+    glLoadIdentity();
+    glRotatef(theta[0],1,0,0);  // rotation about x
+    glRotatef(theta[1],0,1,0); // rotate about y
+    glRotatef(theta[2],0,0,1); // rotate about z
+    if(ch==1)
+    	glutSolidCube(1);
+    if(ch==2)
+    	glutSolidTeapot(0.5);
+    if(ch==3)
+    	glutSolidCone(0.5,0.5,20,20);
+    glFlush();
+    glutSwapBuffers();  // use whenever you use double buffer
+}
+void menu(int id)
+{
+switch(id)
+{
+	case 1:
+		ch=1;
+		break;
+	case 2:
+		ch=2;
+		break;
+	case 3:
+		ch=3;
+		break;
+}
+}
+int main(int argc, char ** argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(500,500);
+    glutCreateWindow("Color Cube");
+    glutCreateMenu(menu);
+	glutAddMenuEntry("Cube",1); 
+	glutAddMenuEntry("Teapot",2);
+	glutAddMenuEntry("Cone",3);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+    glutDisplayFunc(display);
+    glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glMaterialfv(GL_FRONT,GL_AMBIENT,ambient);
+	glLightfv(GL_LIGHT0,GL_POSITION,light_pos);
+    glutMouseFunc(mouse); 
+    glutIdleFunc(idle);
+    glShadeModel(GL_SMOOTH);
+    glEnable(GL_DEPTH_TEST);
+    glutMainLoop();
+    return 0;
+}
+  
